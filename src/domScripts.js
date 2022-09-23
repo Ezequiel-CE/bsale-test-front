08d95eff31b547d501480798f54879ac2fbd3787
Ezequiel <ezequielce490@gmail.com>
2022-09-23 16:09:36 -0300

    feat: added placeholder img for broken url

--- src/domScripts.js
+++ src/domScripts.js
@@ -1,4 +1,5 @@
 import { mainApp } from "./index";
+import placeholder from "./assets/default.jpg";
 
 export const setLoading = () => {
   mainApp.innerHTML = ` <div class="d-flex justify-content-center align-items-center h-100">
@@ -12,9 +13,9 @@ const createCards = (products) => {
   const cardsSTring = products
     .map((product) => {
       return `
-      <div class="col my-3">
-      <div class="card ">
-    <img class="card-img-top" src="${product.url_image}" alt="Card image cap">
+      <div class="col my-3" >
+      <div class="card p-2">
+    <img class="card-img-top" src="${product.url_image}" onerror="this.onerror=null;this.src='${placeholder}'" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">${product.name}</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -28,8 +29,8 @@ const createCards = (products) => {
 
 export const createGallery = (products) => {
   const galleryString = `
-  <div class="text-center container py-5">
-          <h4 class="mt-4 mb-5"><strong>All Products</strong></h4>
+  <div class="text-center container py-5 mt-4">
+          <h4 class="mt-5 mb-5"><strong>All Products</strong></h4>
         </div>
         <div class="container">
   <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3  row-cols-lg-4">
--- src/index.js
+++ src/index.js
@@ -1,44 +1,5 @@
 import { setLoading, createGallery } from "./domScripts";
 
-const productsTest = [
-  {
-    id: 29,
-    name: "RON FLOR DE CAÑA 5 AÑOS",
-    url_image:
-      "https://dojiw2m9tvv09.cloudfront.net/11132/product/flor59677.jpg",
-    price: 4590,
-    discount: 0,
-    category: 3,
-  },
-  {
-    id: 30,
-    name: "RON HAVANA AÑEJO RESERVA",
-    url_image:
-      "https://dojiw2m9tvv09.cloudfront.net/11132/product/havanaan-ejo9750.jpg",
-    price: 6990,
-    discount: 0,
-    category: 3,
-  },
-  {
-    id: 31,
-    name: "RON HAVANA ESPECIAL",
-    url_image:
-      "https://dojiw2m9tvv09.cloudfront.net/11132/product/havanaespecial9768.jpg",
-    price: 5990,
-    discount: 20,
-    category: 3,
-  },
-  {
-    id: 32,
-    name: "RON PAMPERO",
-    url_image:
-      "https://dojiw2m9tvv09.cloudfront.net/11132/product/pampero-especial0296.jpg",
-    price: 5490,
-    discount: 20,
-    category: 3,
-  },
-];
-
 export const mainApp = document.querySelector("#main");
 
 const loadProducts = async () => {
