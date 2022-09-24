b7748c5c3614df89802e84dc43267f93e2e0b36b
Ezequiel <ezequielce490@gmail.com>
2022-09-23 22:57:33 -0300

    feat: added search by category

--- src/domScripts.js
+++ src/domScripts.js
@@ -1,4 +1,4 @@
-import { mainApp } from "./index";
+import { mainApp, dropDown, getByCategory } from "./index";
 import placeholder from "./assets/default.jpg";
 
 export const setLoading = () => {
@@ -14,18 +14,25 @@ const createCards = (products) => {
     .map((product) => {
       return `
       <div class="col my-3 " >
-      <div class="card p-2 h-100 ">
-    <img class="card-img-top position-relative" src="${
-      product.url_image
-    }" onerror="this.onerror=null;this.src='${placeholder}'" alt="Card image cap">
-    ${
-      product.discount > 0 &&
-      '<span class="badge bg-danger ms-2 position-absolute top-10 start-0">-10%</span>'
-    }
+      <div class="card p-2 h-100 d-flex flex-column justify-content-between">
+      <div class="h-75" >
+      <img class="card-img-top position-relative " src="${
+        product.url_image
+      }" onerror="this.onerror=null;this.src='${placeholder}'" alt="Card image cap">
+      ${
+        product.discount > 0
+          ? '<span class="badge bg-danger ms-2 position-absolute top-10 start-0">-10%</span>'
+          : ""
+      }
+      </div>
     <div class="card-body">
-      <h5 class="card-title text-center">${product.name}</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h6 class="card-title text-center">${product.name}</h6>
+      <p class="card-text text-center"> $${product.price}</p>
+      <hr class="my-0" />
+      <div class="d-flex mt-3 mx-0">
+      <button type="button" class="btn btn-secondary flex-fill m-0">Buy now</button>
+      </div>
+
     </div>
   </div></div>`;
     })
@@ -45,4 +52,23 @@ export const createGallery = (products, title) => {
 </div>`;
 
   mainApp.innerHTML = galleryString;
+};
+
+export const createCategories = (categories) => {
+  console.log(categories);
+  const categoriesString = categories
+    .map((cat) => {
+      return `<li class="dropdown-item" role="button" id="${cat.id}">${cat.name}</li>`;
+    })
+    .join("");
+
+  dropDown.innerHTML = categoriesString;
+  const links = [...dropDown.children];
+
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      const { id, textContent } = link;
+      getByCategory(id, textContent);
+    });
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import { setLoading, createGallery } from "./domScripts";
+import { setLoading, createGallery, createCategories } from "./domScripts";
 
+//ELEMENTS
 export const mainApp = document.querySelector("#main");
 const searchForm = document.querySelector("#search-form");
 const input = document.querySelector("#search-input");
+const homeBtn = document.querySelector("#home-btn");
+export const dropDown = document.querySelector("#categories-drop");
 
-const loadProducts = async () => {
+/**
+ * Function for fetching all products
+ */
+
+const loadAllProducts = async () => {
   setLoading();
   try {
     const response = await fetch("http://localhost:5000/api/products");
@@ -21,6 +28,32 @@ const loadProducts = async () => {
   }
 };
 
+/**
+ * Function for fetching all categories
+ */
+
+const loadAllCategories = async () => {
+  setLoading();
+  try {
+    const response = await fetch("http://localhost:5000/api/category");
+
+    if (!response.ok) {
+      throw new Error("cant get category");
+    }
+
+    const data = await response.json();
+
+    createCategories(data.categories);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/**
+ * Function for fetching products by name
+ * @param {string} name
+ */
+
 const searchForProduct = async (name) => {
   setLoading();
   try {
@@ -34,17 +67,49 @@ const searchForProduct = async (name) => {
     }
 
     const data = await response.json();
-    console.log(data);
+
     createGallery(data.products, `Results for "${name}"`);
   } catch (error) {
     console.log(error);
   }
 };
 
-loadProducts();
+/**
+ * Function for fetching products by category
+ * @param {string} id
+ * @param {string} name
+ */
+
+export const getByCategory = async (id, name) => {
+  setLoading();
+  try {
+    const response = await fetch("http://localhost:5000/api/category/" + id);
+
+    if (!response.ok) {
+      throw new Error("cant fetch category");
+    }
+
+    const data = await response.json();
+
+    createGallery(data.products, `${name} category`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const init = () => {
+  loadAllProducts();
+  loadAllCategories();
+};
+
+init();
+
+//Listeners
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchForProduct(input.value);
   input.value = "";
 });
+
+homeBtn.addEventListener("click", loadAllProducts);
